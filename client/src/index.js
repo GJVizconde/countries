08b2906d0fefd05e9574b971b4d4b38905861445
GJVizconde <gilmerjose.vizconde2@gmail.com>
2023-05-29 18:35:52 -0500

    push

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3001";
 
-axios.defaults.baseURL = "countries-production-a897.up.railway.app/";
+axios.defaults.baseURL = "countries-production-a897.up.railway.app";
 
 ReactDOM.render(
   <Provider store={store}>
